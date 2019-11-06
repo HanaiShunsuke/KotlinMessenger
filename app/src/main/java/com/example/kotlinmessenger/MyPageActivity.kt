44008c27804cd106e66ab2fec9ae124161a5d582
enPiT-P10 <d913@oshima-k.ac.jp>
2019-11-06 19:03:27 +0900

    マイページ画面に写真と名前を表示できるようにした VCS→Git→rebase my github forkから更新できる

--- app/src/main/java/com/example/kotlinmessenger/MyPageActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MyPageActivity.kt
@@ -2,12 +2,24 @@ package com.example.kotlinmessenger
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlinmessenger.messages.LatestMessagesActivity
+import com.example.kotlinmessenger.models.User
 import com.example.kotlinmessenger.trades.ProductListActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_my_page.*
 
 class MyPageActivity : AppCompatActivity() {
+    companion object{
+        var currentUser: User? = null
+        val TAG = "LatestMessages"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +32,29 @@ class MyPageActivity : AppCompatActivity() {
             val intent = Intent(this, ProductListActivity::class.java)
             startActivity(intent)
         }
+        fetchCurrentUser()
+
+
+
+    }
+    private fun fetchCurrentUser(){
+        val uid = FirebaseAuth.getInstance().uid
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                currentUser = p0.getValue(User::class.java)
+                username_mypage_text.text= currentUser?.username
+                Picasso.get().load(currentUser?.profileImageUrl).into(mypage_imageview)
+                Log.d("LatestMessages","Current user ${currentUser?.profileImageUrl}")
+
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
     }
 }
+
+
+
