c89df649208538992d6a39a5c775c7cd7d1c839c
enPiT-P9 <enPiTT9.2019@gmail.com>
2019-12-12 15:22:32 +0900

    レイアウトしました。一つだけです

--- app/src/main/java/com/example/kotlinmessenger/MypageEditActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MypageEditActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinmessenger
 
 import android.content.Intent
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -14,20 +15,25 @@ class MypageEditActivity : AppCompatActivity() {
 
         val button=findViewById<Button>(R.id.button)
 
-        button.setOnClickListener{
+        button.setOnClickListener {
+            val pref= PreferenceManager.getDefaultSharedPreferences(this)
             val editText = findViewById<EditText>(R.id.editText)
             val editText2 = findViewById<EditText>(R.id.editText2)
 
             val message = editText.run { text.toString() }
             val message2 = editText2.run { text.toString() }
 
-            val intent = Intent(this,MyPageActivity::class.java)
+            val intent = Intent(this, MyPageActivity::class.java)
 
-            intent.putExtra("message",message)
-            intent.putExtra("message2",message2)
+            intent.putExtra("message", message)
+            intent.putExtra("message2", message2)
+
 
             startActivity(intent)
         }
+
+
+
 
     }
 
